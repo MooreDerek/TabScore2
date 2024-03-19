82aa88f93ddc58bd33c30cb3b6b213a1d3094fa2
Peter Flippant <peterflippant@ntlworld.com>
2024-03-19 08:51:24 +0000

    1.0.0.1 Remove Elmah

--- TabScore2/Program.cs
+++ TabScore2/Program.cs
@@ -1,8 +1,6 @@
 // TabScore2, a wireless bridge scoring program.  Copyright(C) 2024 by Peter Flippant
 // Licensed under the Apache License, Version 2.0; you may not use this file except in compliance with the License
 
-using ElmahCore;
-using ElmahCore.Mvc;
 using TabScore2.Classes;
 using TabScore2.DataServices;
 using TabScore2.Forms;
@@ -30,7 +28,6 @@ namespace TabScore2
             webAppBuilder.Services.AddControllersWithViews();
             webAppBuilder.Services.AddWebOptimizer(options => { options.EnableDiskCache = false; });
             string logPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            webAppBuilder.Services.AddElmah<XmlFileErrorLog>(options => { options.LogPath = logPath; }); 
             webAppBuilder.Services.AddSingleton<IUtilities, Utilities>();
             webAppBuilder.Services.AddSingleton<IDatabase, BwsDatabase>();
             webAppBuilder.Services.AddSingleton<IExternalNamesDatabase, ExternalNamesDatabase>();
@@ -39,7 +36,6 @@ namespace TabScore2
             webAppBuilder.Services.AddHttpContextAccessor();
             WebApplication webApp = webAppBuilder.Build();
             webApp.UseExceptionHandler("/ErrorScreen/Index");
-            webApp.UseElmah();
             webApp.UseWebOptimizer();
             webApp.UseStaticFiles();
             webApp.UseRouting();
